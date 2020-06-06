dec169342a6c8e5274ec2b4a7c6947169cb9475c
Nikhil569 <48991799+Nikhil569@users.noreply.github.com>
2020-06-06 23:34:56 +0530

    Add files via upload

---
+++ PrettyPrint.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int ** preetyPrint(int a){
+    int** arr = new int*[2*a-1];
+    for(int i = 0; i < 2*a-1; i++){
+        arr[i] = new int[2*a-1];}
+    
+    for(int i=0;i<a;i++){
+        for(int j=i;j<=2*(a-i-1)+i;j++){
+            arr[i][j] = a-i;
+            arr[2*(a-i-1)+i][j] = a-i;
+            arr[j][i] = a-i;
+            arr[j][2*(a-i-1)+i] = a-i;
+        }
+    }
+    
+    return arr;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int** a = preetyPrint(n);
+	
+	for (int h = 0; h < 2*n-1; h++)
+      {
+            for (int w = 0; w < 2*n-1; w++)
+            {
+                  cout<<a[h][w]<<" ";
+            }
+            cout<<endl;
+      }
+	
+	return 0;
+}
