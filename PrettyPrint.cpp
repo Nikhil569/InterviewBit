dd27653dd56a9394a7d8e0c4590c36f6dedeb345
Nikhil569 <48991799+Nikhil569@users.noreply.github.com>
2020-06-06 23:37:59 +0530

    Update PrettyPrint.cpp

--- PrettyPrint.cpp
+++ PrettyPrint.cpp
@@ -6,8 +6,8 @@ int ** preetyPrint(int a){
     for(int i = 0; i < 2*a-1; i++){
         arr[i] = new int[2*a-1];}
     
-    for(int i=0;i<a;i++){
-        for(int j=i;j<=2*(a-i-1)+i;j++){
+    for(int i=0;i<a;i++){				//using i as a level indicator
+        for(int j=i;j<=2*(a-i-1)+i;j++){		//every level has a same kind of cube of no. shifting each value by i and a' = a-i 
             arr[i][j] = a-i;
             arr[2*(a-i-1)+i][j] = a-i;
             arr[j][i] = a-i;
